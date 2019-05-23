05621be3cefaf587620f0897b78ba4154e74a5df
Vincent Ninh DO <vincentnhdo@gmail.com>
2019-05-23 16:30:52 -0700

    Add all lab materials

---
+++ labs/lab11/lab11/starter/assets/js/script.js
@@ -0,0 +1,32 @@
+$(document).ready(function()  {
+
+	// PART 1: YOUR CODE HERE
+
+	$(".nav-item").click(function() {
+
+		let buttonName = $(this).text();
+		let buttonActive = $(this).hasClass("active");
+
+		// UNCOMMENT THE SECTION BELOW FOR PART 2
+		
+		/*
+		
+		// YOUR CODE HERE - PART 2.1
+
+		if (condition) {
+			$(this).addClass("active");
+			$(this).removeClass("inactive");
+
+			// YOUR CODE HERE - PART 2.2
+
+		} else {
+			$(this).addClass("inactive");
+			$(this).removeClass("active");
+
+			// YOUR CODE HERE - PART 2.2
+
+		} 
+		*/
+		
+	})
+})
